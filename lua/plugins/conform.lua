d0224d852e54699e3cf1bd59c2ec465062d1fbc0
hieuchan <chanerdame@gmail.com>
2023-11-19 01:18:05 +0700

    refactor structor

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,33 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>fb",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			-- ["typescript"] = { "prettier" },
+			["typescriptreact"] = { "prettier" },
+			["css"] = { "prettier" },
+			["scss"] = { "prettier" },
+			["less"] = { "prettier" },
+			["html"] = { "prettier" },
+			["json"] = { "prettier" },
+			["jsonc"] = { "prettier" },
+			["yaml"] = { "prettier" },
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+}
