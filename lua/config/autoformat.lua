ef5faafdd8960bf933b5789a2cb36060d6107958
hieuchan <chanerdame@gmail.com>
2023-11-19 10:01:38 +0700

    refactor autoformat

--- lua/config/autoformat.lua
+++ lua/config/autoformat.lua
@@ -1,91 +1,231 @@
--- autoformat.lua
---
--- Use your language server to automatically format your code on save.
--- Adds additional commands as well to manage the behavior
+local api = vim.api
 
-return {
-  'neovim/nvim-lspconfig',
-  config = function()
-    -- Switch for controlling whether you want autoformatting.
-    --  Use :KickstartFormatToggle to toggle autoformatting on or off
-    local format_is_enabled = true
-    vim.api.nvim_create_user_command('KickstartFormatToggle', function()
-      format_is_enabled = not format_is_enabled
-      print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-    end, {})
+local colors = {
+  fg = "#76787d",
+  bg = "#252829",
+}
 
-    -- Create an augroup that is used for managing our formatting autocmds.
-    --      We need one augroup per client to make sure that multiple clients
-    --      can attach to the same buffer without interfering with each other.
-    local _augroups = {}
-    local get_augroup = function(client)
-      if not _augroups[client.id] then
-        local group_name = 'kickstart-lsp-format-' .. client.name
-        local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-        _augroups[client.id] = id
-      end
+-- don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
-      return _augroups[client.id]
+--- Remove all trailing whitespace on save
+-- local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
+-- api.nvim_create_autocmd("BufWritePre", {
+--   command = [[:%s/\s\+$//e]],
+--   group = TrimWhiteSpaceGrp,
+-- })
+
+-- wrap words "softly" (no carriage return) in mail buffer
+api.nvim_create_autocmd("Filetype", {
+  pattern = "mail",
+  callback = function()
+    vim.opt.textwidth = 0
+    vim.opt.wrapmargin = 0
+    vim.opt.wrap = true
+    vim.opt.linebreak = true
+    vim.opt.columns = 80
+    vim.opt.colorcolumn = "80"
+  end,
+})
+
+-- Highlight on yank
+api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- go to last loc when opening a buffer
+api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = api.nvim_buf_get_mark(0, '"')
+    local lcount = api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- Enable spell checking for certain file types
+api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en,de"
+    end,
+  }
+)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<leader>v', "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)
+  end,
+})
+
+
+api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    -- change the background color of floating windows and borders.
+    vim.cmd('highlight NormalFloat guibg=none guifg=none')
+    vim.cmd('highlight FloatBorder guifg=' .. colors.fg .. ' guibg=none')
+    vim.cmd('highlight NormalNC guibg=none guifg=none')
+
+    -- change neotree background colors
+    -- Default: NeoTreeNormal  xxx ctermfg=223 ctermbg=232 guifg=#d4be98 guibg=#141617
+    -- vim.cmd('highlight NeoTreeNormal guibg=#252e33 guifg=none')
+    -- vim.cmd('highlight NeoTreeFloatNormal guifg=none guibg=none')
+    -- vim.cmd('highlight NeoTreeFloatBorder gui=none guifg=' .. colors.fg .. ' guibg=none')
+    -- vim.cmd('highlight NeoTreeEndOfBuffer guibg=#252e33') -- 1d2021
+  end,
+})
+
+-- close some filetypes with <q>
+api.nvim_create_autocmd("FileType", {
+  group = api.nvim_create_augroup("close_with_q", { clear = true }),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- resize neovim split when terminal is resized
+api.nvim_command('autocmd VimResized * wincmd =')
+
+-- api.nvim_create_autocmd("ColorScheme", {
+--   pattern = "kanagawa",
+-- callback = function()
+--   if vim.o.background == "light" then
+--     vim.fn.system("kitty +kitten themes Kanagawa_light")
+--   elseif vim.o.background == "dark" then
+--     vim.fn.system("kitty +kitten themes Kanagawa_dragon")
+--   else
+--     vim.fn.system("kitty +kitten themes Kanagawa")
+--   end
+-- end,
+-- })
+
+
+--fix terraform and hcl comment string
+api.nvim_create_autocmd("FileType", {
+  group = api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
+
+-- api.nvim_create_autocmd({ "OptionSet" }, {
+--   pattern = { "background" },
+--   callback = function(ev)
+--     if vim.o.background == 'dark' then
+--       vim.cmd("colorscheme gruvbox-material")
+--     else
+--       vim.cmd("colorscheme gruvbox-material")
+--     end
+--     vim.cmd("mode")
+--   end
+-- })
+
+local _augroups = {}
+local get_augroup = function(client)
+  if not _augroups[client.id] then
+    local group_name = 'kickstart-lsp-format-' .. client.name
+    local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+    _augroups[client.id] = id
+  end
+
+  return _augroups[client.id]
+end
+
+-- See `:help LspAttach` for more information about this autocmd event.
+api.nvim_create_autocmd('LspAttach', {
+  group = api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
+  -- This is where we attach the autoformatting for reasonable clients
+  callback = function(args)
+    local client_id = args.data.client_id
+    local client = vim.lsp.get_client_by_id(client_id)
+    local bufnr = args.buf
+
+    -- Only attach to clients that support document formatting
+    if not client.server_capabilities.documentFormattingProvider then
+      return
     end
 
-    -- Whenever an LSP attaches to a buffer, we will run this function.
-    --
-    -- See `:help LspAttach` for more information about this autocmd event.
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
-      -- This is where we attach the autoformatting for reasonable clients
-      callback = function(args)
-        local client_id = args.data.client_id
-        local client = vim.lsp.get_client_by_id(client_id)
-        local bufnr = args.buf
-
-        -- Only attach to clients that support document formatting
-        if not client.server_capabilities.documentFormattingProvider then
-          return
-        end
-
-        -- Tsserver usually works poorly. Sorry you work with bad languages
-        -- You can remove this line if you know what you're doing :)
-        -- Config for eslint
-        if client.name == 'tsserver' then
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            callback = function(event)
-              local eslint_client = vim.lsp.get_active_clients({ bufnr = event.buf, name = 'eslint' })[1]
-              if eslint_client then
-                local diag = vim.diagnostic.get(
-                  event.buf,
-                  { namespace = vim.lsp.diagnostic.get_namespace(eslint_client.id) }
-                )
-                if #diag > 0 then
-                  vim.cmd('EslintFixAll')
-                end
-              end
-              -- config for prettier with conform.nvim
-              -- require("conform").format({ bufnr = event.buf })
-            end,
-          })
-          return
-        end
-
-        -- Create an autocmd that will run *before* we save the buffer.
-        --  Run the formatting command for the LSP that has just attached.
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          group = get_augroup(client),
-          buffer = bufnr,
-          callback = function()
-            if not format_is_enabled then
-              return
+    -- Tsserver usually works poorly. Sorry you work with bad languages
+    -- You can remove this line if you know what you're doing :)
+    -- Config for eslint
+    if client.name == 'tsserver' then
+      api.nvim_create_autocmd('BufWritePre', {
+        callback = function(event)
+          local eslint_client = vim.lsp.get_active_clients({ bufnr = event.buf, name = 'eslint' })[1]
+          if eslint_client then
+            local diag = vim.diagnostic.get(
+              event.buf,
+              { namespace = vim.lsp.diagnostic.get_namespace(eslint_client.id) }
+            )
+            if #diag > 0 then
+              vim.cmd('EslintFixAll')
             end
+          end
+          -- config for prettier with conform.nvim
+          -- require("conform").format({ bufnr = event.buf })
+        end,
+      })
+      return
+    end
 
-            vim.lsp.buf.format {
-              async = false,
-              filter = function(c)
-                return c.id == client.id
-              end,
-            }
+    -- Create an autocmd that will run *before* we save the buffer.
+    --  Run the formatting command for the LSP that has just attached.
+    api.nvim_create_autocmd('BufWritePre', {
+      group = get_augroup(client),
+      buffer = bufnr,
+      callback = function()
+        if not format_is_enabled then
+          return
+        end
+
+        vim.lsp.buf.format {
+          async = false,
+          filter = function(c)
+            return c.id == client.id
           end,
-        })
+        }
       end,
     })
   end,
-}
+})
