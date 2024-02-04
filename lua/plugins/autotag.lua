daf8f8d77c50f3cce57629cfe91ff2200ad8a2f8
hieuchan <chanerdame@gmail.com>
2024-02-05 00:25:13 +0700

    add: autopairs plugin

---
+++ lua/plugins/autotag.lua
@@ -0,0 +1,12 @@
+return {
+	"windwp/nvim-ts-autotag",
+	opts = {
+		autotag = {
+			enable = true,
+			nable_rename = true,
+			enable_close = true,
+			enable_close_on_slash = true,
+			filetypes = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+		}
+	}
+}
