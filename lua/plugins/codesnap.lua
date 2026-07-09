62138feb67244dabd40f29da074376d8406ddb5b
hieuchan <chanerdame@gmail.com>
2026-07-09 09:02:34 +0700

    chore: upgrade codesnap build

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,7 +1,8 @@
 return {
 	"mistricky/codesnap.nvim",
 	version = "1.1.5",
-	build = "make build_generator",
+	-- cargo update -p time first: pinned time 0.3.34 fails E0282 on rustc >=1.80, breaking the build.
+	build = "cargo update -p time --manifest-path generator/Cargo.toml && make build_generator",
 	lazy = true,
 	cmd = "CodeSnap",
 	opts = {
