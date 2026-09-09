097660b7fd7e698f74fd4112cbe57feb53c7cef6
hieuchan <chanerdame@gmail.com>
2026-09-09 11:08:13 +0700

    chore: disabled copilot

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,30 +1,3 @@
-local function biome_lsp_or_prettier(bufnr)
-	local has_biome_lsp = vim.lsp.get_clients({
-		bufnr = bufnr,
-		name = "biome",
-	})[1]
-	if has_biome_lsp then
-		return { "biome" }
-	end
-	local has_prettier = vim.fs.find({
-		-- https://prettier.io/docs/en/configuration.html
-		".prettierrc",
-		".prettierrc.json",
-		".prettierrc.yml",
-		".prettierrc.yaml",
-		".prettierrc.json5",
-		".prettierrc.js",
-		".prettierrc.cjs",
-		".prettierrc.toml",
-		"prettier.config.js",
-		"prettier.config.cjs",
-	}, { upward = true })[1]
-	if has_prettier then
-		return { "prettier" }
-	end
-	return { "biome" }
-end
-
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
